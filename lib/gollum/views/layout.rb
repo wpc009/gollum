243e206d9d73cf1957c0bf138b2d9d355279c405
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2012-05-09 16:01:13 +0200

    Add links and status for omniauth

--- lib/gollum/views/layout.rb
+++ lib/gollum/views/layout.rb
@@ -32,6 +32,18 @@ module Precious
         @js
       end
 
+      # Passthrough additional omniauth parameters for status bar
+      def user_authed
+        @user_authed
+      end
+      
+      def user_provider
+        @user.provider
+      end
+      
+      def user_name
+        @user.name
+      end
     end
   end
 end
